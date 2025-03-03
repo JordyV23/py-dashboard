f6e22605a873f9665c1c68088366245764250883
JordyV23 <jordyvargas0123@gmail.com>
2025-03-02 22:03:18 -0600

    Se agregan graficos interactivos

--- main.py
+++ main.py
@@ -28,33 +28,82 @@ with st.expander("Introduccion", expanded=True):
     
 try: 
     # Carga de los datasets
-    prog_df = pd.read_csv('data/prs.csv')
+    prog_df = pd.read_csv('data/prs.csv').query('year == 2022')
     iris_df = pd.read_csv('data/Iris.csv')
     st.success("✅Datos cargados exitosamente")
     
     # Visualizar datos
     st.header("🎨 Visualizaciones con Matplotlib")
-    
     # Se crea un contenedor
     with st.container():
         col1,col2 = st.columns(2)
         with col1:
             st.subheader("Grafico de Dispersion")
-            
             fig,ax = plt.subplots(figsize=(8,6)) # fig contiene la figura y ax los eje
             languges = prog_df['name'].head(5)
             users = prog_df['count'].head(5)
-            
             ax.scatter(languges,users,color='blue', alpha=0.6)
             plt.xticks(rotation=45)
             plt.title('Cantitdad de PRs por Lenguaje de Programacion')
             plt.xlabel('Lenguaje')
             plt.ylabel('Cantidad de PRs')
             st.pyplot(fig)
+            plt.close()
             
         with col2:
             st.subheader("Grafico de Barras")
+            fig,ax = plt.subplots(figsize=(8,6)) # fig contiene la figura y ax los eje
+            languges = prog_df['name'].head(5)
+            users = prog_df['count'].head(5)
+            ax.bar(languges,users,color='skyblue')
+            plt.xticks(rotation=45)
+            plt.title('Cantitdad de PRs por Lenguaje de Programacion')
+            plt.xlabel('Lenguaje')
+            plt.ylabel('Cantidad de PRs')
+            st.pyplot(fig)
+            plt.close()
+    
+    # Visualizar datos con Searborn
+    st.header("🎨 Visualizaciones con Seaborn")
+     # Se crea un contenedor
+    with st.container():
+        col1,col2 = st.columns(2)
+        with col1:
+            st.subheader("Grafico de Violin")
+            fig,ax = plt.subplots(figsize=(8,6)) # fig contiene la figura y ax los eje
+            sns.violinplot(data=iris_df, x='Species', y='SepalLengthCm')
+            plt.xticks(rotation=45)
+            plt.title('Distribuicion de longitud del Sepalo por Especie')
+            st.pyplot(fig)
+            plt.close()
+        with col2:
+            st.subheader("Grafico de Cajas")
+            fig,ax = plt.subplots(figsize=(8,6)) # fig contiene la figura y ax los eje
+            sns.boxplot(data=iris_df, x='Species', y='PetalLengthCm')
+            plt.xticks(rotation=45)
+            plt.title('Estadistica de longitud del Petalo por Especie')
+            st.pyplot(fig)
+            plt.close()
             
+    
+    # Visualizar datos de manera interactiva
+    st.header("🎨 Visualizaciones Interactivas")
+    # Grafico de lineas interactivo
+      # Se crea un contenedor
+    with st.container():
+        fig = px.line(prog_df,
+                      x='name',
+                      y='count',
+                      title='Tendencia de uso',
+                      markers=True)
+        st.plotly_chart(fig,use_container_width=True)
+        
+        
+        fig = px.pie(prog_df,
+                      names='name',
+                      values='count',
+                      title='Tendencia de uso')
+        st.plotly_chart(fig,use_container_width=True)
     
 except Exception as e:
     st.error(f"Error al cargar los datos: {str(e)}")
